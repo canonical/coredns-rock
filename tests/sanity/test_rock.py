f3229cd845145d438e6364e5ca87a314fedadcb9
Maciej Golaszewski <maciej.golaszewski@canonical.com>
2024-08-21 16:59:07 +0200

    added test sanity and gitignore file

---
+++ tests/sanity/test_rock.py
@@ -0,0 +1,18 @@
+#
+# Copyright 2024 Canonical, Ltd.
+#
+
+import pytest
+from k8s_test_harness.util import docker_util, env_util
+
+
+@pytest.mark.parametrize("image_version", ["1.11.1"])
+def test_sanity(image_version):
+    rock = env_util.get_build_meta_info_for_rock_version(
+        "coredns", image_version, "amd64"
+    )
+    image = rock.image
+    entrypoint = "/coredns"
+
+    process = docker_util.run_in_docker(image, [entrypoint, "--version"])
+    assert f"CoreDNS-{image_version}" in process.stdout
