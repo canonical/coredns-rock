3f49b61ff7f9e7bfcd6f854a7eedc95f0c0db80e
Adam Dyess <adam.dyess@canonical.com>
2024-10-17 12:42:46 -0500

    Avoid hardcoding version in the sanity test

--- tests/sanity/test_rock.py
+++ tests/sanity/test_rock.py
@@ -2,11 +2,17 @@
 # Copyright 2024 Canonical, Ltd.
 #
 
+from pathlib import Path
+
 import pytest
+import yaml
 from k8s_test_harness.util import docker_util, env_util
 
+ROCKCRAFT = Path(__file__).parent.parent.parent / "rockcraft.yaml"
+VERSION = yaml.safe_load(ROCKCRAFT.open())["version"]
 
-@pytest.mark.parametrize("image_version", ["1.11.3"])
+
+@pytest.mark.parametrize("image_version", [VERSION])
 def test_sanity(image_version):
     rock = env_util.get_build_meta_info_for_rock_version(
         "coredns", image_version, "amd64"
