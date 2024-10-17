dd92ba885ca90e03ed9d4c72906c426d565ee499
Adam Dyess <adam.dyess@canonical.com>
2024-10-17 12:35:56 -0500

    Test 1.11.3 image runs

--- tests/sanity/test_rock.py
+++ tests/sanity/test_rock.py
@@ -6,7 +6,7 @@ import pytest
 from k8s_test_harness.util import docker_util, env_util
 
 
-@pytest.mark.parametrize("image_version", ["1.11.1"])
+@pytest.mark.parametrize("image_version", ["1.11.3"])
 def test_sanity(image_version):
     rock = env_util.get_build_meta_info_for_rock_version(
         "coredns", image_version, "amd64"
